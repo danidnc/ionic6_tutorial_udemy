e27b3a6509220a7848c72c50c3b46dc30d9e299b
danidnc <danieldnc@outlook.es>
2021-06-17 14:19:56 +0200

    fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  ngOnInit() {
+    const numero = 10;
+
+    console.log('Hola Mundo ', numero);
+  }
 }
